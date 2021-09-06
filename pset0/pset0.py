3c5ee3e393f9c90c883d49f6bd66367195bd9ca3
BarshaC <barsha.chaudhary@bison.howard.edu>
2021-09-06 13:17:24 -0400

    added comments to pset0.py

--- pset0/pset0.py
+++ pset0/pset0.py
@@ -3,6 +3,7 @@ def intro():
     last_name = str(raw_input("**"))   #Take input from user and saves it in last_name
     print ("Enter your first name:") #Similar as line 2
     first_name = str(raw_input("**"))  #changes the input to string #str makes sure it takes string input only
-    print (first_name)
-    print (last_name)
+    print (first_name) #printing my first name
+    print (last_name)  #printing my last name
 intro()
+
